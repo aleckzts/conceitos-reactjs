d8cc71c8f9960fb73fbfa3714d3b85392cfc0169
Aleck Zanderbier <aleckzts@gmail.com>
2020-06-17 23:32:47 -0300

    retirado comentarios

--- src/App.js
+++ src/App.js
@@ -43,13 +43,6 @@ function App() {
           </button>
           </li>
         ))}
-        {/* <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li> */}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
